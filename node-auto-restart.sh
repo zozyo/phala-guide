56dadb1dc20c4588c2ae71cb1b38b7bb23a21626
zozyo <zzy.touhou@gmail.com>
2022-03-18 12:38:05 +0800

    Update node-auto-restart.sh

--- node-auto-restart.sh
+++ node-auto-restart.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#restart commands
+function restartNode(){
+	echo "检测到卡顿超时！重启节点！" 		#可替换为各种告警脚本命令
+	phala stop node 						#停止节点命令，取决于用户部署环境
+	phala start 							#启动节点命令，取决于用户部署环境
+}
+
+#update commands
+function updateNode() {
+	echo "重启多次无效！更新节点！" 			#可替换为各种告警脚本命令
+	phala stop node 						#停止节点命令，取决于用户部署环境
+	docker image rm phalanetwork/khala-node	#移除旧 node 镜像
+	docker pull phalanetwork/khala-node		#拉取新 node 镜像
+	phala start 							#启动节点命令，取决于用户部署环境
+}
+
+#check if synced
 function isSynced(){
 	if [ -z $1 ]; then
 		echo "未启动"
@@ -110,8 +127,7 @@ while true; do
 
 	#if stuck too long, restart node
 	if [ $node_stuck_count -ge $stuck_times ]; then
-		echo "检测到卡顿超时！重启节点！" 		#可替换为各种告警脚本命令
-		docker restart phala-node
+		restartNode
 		restart_count=`expr $restart_count + 1`
 		node_stuck_count=0
 		#waiting 5 mins for node fully restarted
@@ -124,11 +140,7 @@ while true; do
 
 	#if restart not work, try update node
 	if [ $restart_count -ge $restart_times ]; then
-		echo "重启多次无效！更新节点！" 		#可替换为各种告警脚本命令
-		phala stop node 			#停止节点命令，取决于用户部署环境
-		docker image rm phalanetwork/khala-node
-		docker pull phalanetwork/khala-node
-		phala start 				#启动节点命令，取决于用户部署环境
+		updateNode
 		restart_count=0
 		#waiting 5 mins for node fully restarted
 		for i in `seq 300 -1 1`
