705296d2bd9fdbfe4735f28f36149fdda4eb0e6e
zozyo <zzy.touhou@gmail.com>
2022-04-11 00:21:43 +0800

    Update vip.sh

--- vip.sh
+++ vip.sh
@@ -7,4 +7,4 @@ echo 1 > /proc/sys/net/ipv4/conf/lo/arp_ignore
 echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce
 echo 2 > /proc/sys/net/ipv4/conf/lo/arp_announce
 ifconfig $dev $vip netmask 255.255.255.255 broadcast $vip up
-route add -host $vip lo:0
+route add -host $vip lo:1
